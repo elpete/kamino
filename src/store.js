232c1394f6034a5ef45375abb671714eac607478
Mike Burt <mike.burt86@gmail.com>
2016-07-21 13:51:58 -0600

    comment out the Save and Load store

--- src/store.js
+++ src/store.js
@@ -1,27 +1,27 @@
 import configureStore from './stores/configureStore'
 
 function loadState() {
-    try {
-        const serializedState = localStorage.getItem('state')
-        if (serializedState === null) {
-            return undefined
-        }
-        return JSON.parse(serializedState)
-    }
-    catch (err) {
-        console.error(err)
-        return undefined
-    }
+    // try {
+    //     const serializedState = localStorage.getItem('state')
+    //     if (serializedState === null) {
+    //         return undefined
+    //     }
+    //     return JSON.parse(serializedState)
+    // }
+    // catch (err) {
+    //     console.error(err)
+    //     return undefined
+    // }
 }
 
 function saveState(state) {
-    try {
-        const serializedState = JSON.stringify(state)
-        localStorage.setItem('state', serializedState)
-    }
-    catch (err) {
-        console.error(err)
-    }
+    // try {
+    //     const serializedState = JSON.stringify(state)
+    //     localStorage.setItem('state', serializedState)
+    // }
+    // catch (err) {
+    //     console.error(err)
+    // }
 
 }
 
