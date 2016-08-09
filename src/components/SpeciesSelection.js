fe94dd36432c054c20999da3304ecda7be8e3dd3
Mike Burt <mike.burt86@gmail.com>
2016-08-08 19:41:07 -0600

    Style changes to Species Selection

--- src/components/SpeciesSelection.js
+++ src/components/SpeciesSelection.js
@@ -1,12 +1,15 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {hashHistory} from 'react-router'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { hashHistory } from 'react-router'
 
 //REDUCERS
-import {getCharacter} from '../reducers/index'
+import { getCharacter } from '../reducers/index'
 
 //ACTIONS
-import {changeCharacterSpecies, changeCharacterCharacteristics} from '../actions/actions'
+import { 
+    changeCharacterSpecies, 
+    changeCharacterCharacteristics
+} from '../actions/actions'
 
 //COMPONENTS
 import Characteristics from './Characteristics'
@@ -46,7 +49,14 @@ export default class SpeciesSelection extends Component {
 
     render() {
         const {species} = this.props.character
-        const {brawn, agility, intellect, cunning, willpower, presence} = this.props.character.characteristics
+        const {
+            brawn, 
+            agility, 
+            intellect, 
+            cunning, 
+            willpower, 
+            presence
+        } = this.props.character.characteristics
         const speciesOptions = Object.keys(speciesRef).map(key => (
             <option value={key} key={key}>
                 {speciesRef[key].display_name}
@@ -57,95 +67,85 @@ export default class SpeciesSelection extends Component {
         const base_strain = speciesRef[species].base_strain_threshold;
         const starting_xp = speciesRef[species].starting_xp;
 
-
-        const activeAbilities = (speciesRef[species].special_abilities.active).map(key => (
-            <li value={key} key={key}>
-                {key}
-            </li>
-        ));
-
-        const passiveAbilities = (speciesRef[species].special_abilities.passive).map(key => (
-            <li value={key} key={key}>
-                {key}
-            </li>
-        ));
-
         return (
             
-            <div className='well'>
-                <form>
-                    <div className='form-group'>
-                        <label>Select a Species</label>
+            <div className = "well">
+                <div className = "row form-group">
+                    <h3 className = "text-center">Select a Species</h3>
+                    <div className = "col-sm-offset-3 col-sm-6">
                         <select 
-                            className='form-control'
+                            className = "form-control"
                             ref = { select => this.speciesSelect = select }
                             onChange = { this.handleChange.bind( this ) } 
-                            value={species}>
-                                {speciesOptions}
+                            value = { species }
+                        >
+                            { speciesOptions }
                         </select>
                     </div>
-                    <div className="well">
-                        <h5>Description</h5>
-                        <p>{speciesRef[species].description}</p>
-                        <h5>Starting Characteristics</h5>
-                        <Characteristics
-                            brawn={brawn}
-                            agility={agility}
-                            intellect={intellect}
-                            cunning={cunning}
-                            willpower={willpower}
-                            presence={presence} />
-                        
-                        <div className='row'>
-                            <div className='col-sm-4'>
-                                <h5>Wound Threshold</h5>
-                                <p>{base_wound} + Brawn</p>
-                            </div>
-                            <div  className='col-sm-4'>
-                                <h5>Strain Threshold</h5>
-                                <p>{base_strain} + Willpower</p>
-                            </div>
-                            <div  className='col-sm-4'>
-                                <h5>Starting XP</h5>
-                                <p>{starting_xp}</p>
-                            </div>
+                </div>
+                <div className = "well">
+
+                    <div className = "row text-center">
+                        <div className = "col-sm-6">
+                            <h4>Description</h4>
+                            <p>{ speciesRef[species].description }</p>
                         </div>
-                        
-                        
-                        <h5>Active Abilities</h5>
-                        <ul>
-                            {activeAbilities}
-                        </ul>
-                        
-                        <h5>Passive Abilities</h5>
-                        <ul>
-                           {passiveAbilities}
-                        </ul>
+                        <div className = "col-sm-6">
+                            <h4>Abilities</h4>
+                            <p><em>Coming Soon...</em></p>
+                        </div>
                     </div>
 
-                    <button 
-                        onClick={ this.toUser.bind( this ) } 
-                        className='btn btn-primary'> 
-                        <span className="glyphicon glyphicon-menu-left"></span> Back to User Name
-                    </button>
-                    <button 
-                        onClick={ this.toCareer.bind( this ) } 
-                        className='btn btn-primary pull-right'> 
-                        Continue to Career Selection <span className="glyphicon glyphicon-menu-right"></span>
-                    </button>
-                </form>
+                    <div className = "row">
+                        <h4 className = "text-center">Starting Characteristics</h4>
+                        <Characteristics
+                            brawn = { brawn }
+                            agility = { agility }
+                            intellect = { intellect }
+                            cunning = { cunning }
+                            willpower = { willpower }
+                            presence = { presence } 
+                        />
+                    </div>
+
+                    <div className = "row text-center">
+                        <div className = "col-sm-4">
+                            <h5>Wound Threshold</h5>
+                            <h4> <span className = "label label-default">{ base_wound } + Brawn</span></h4>
+                        </div>
+                        <div  className = 'col-sm-4'>
+                            <h5>Strain Threshold</h5>
+                            <h4><span className = "label label-default">{ base_strain } + Willpower</span></h4>
+                        </div>
+                        <div  className = 'col-sm-4'>
+                            <h5>Starting XP</h5>
+                            <h4><span className = "label label-default">{ starting_xp }</span></h4>
+                        </div>
+                    </div>
+                </div>
+
+                <button 
+                    onClick = { this.toUser.bind( this ) } 
+                    className = "btn btn-primary"> 
+                    <span className = "glyphicon glyphicon-menu-left"></span> Back to User Name
+                </button>
+                <button 
+                    onClick={ this.toCareer.bind( this ) } 
+                    className = "btn btn-primary pull-right"> 
+                    Continue to Career Selection <span className = "glyphicon glyphicon-menu-right"></span>
+                </button>
             </div>
         )
     }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps( state ) {
     return {
-        character: getCharacter(state)
+        character: getCharacter( state )
     };
 }
 
-export default connect(mapStateToProps, { 
+export default connect( mapStateToProps, { 
     changeCharacterSpecies, 
     changeCharacterCharacteristics 
     } )( SpeciesSelection )
