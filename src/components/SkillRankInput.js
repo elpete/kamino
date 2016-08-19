425dda34b88ce5b0d246fbd5a120e2c247459c6e
Mike Burt <mike.burt86@gmail.com>
2016-08-18 21:20:13 -0600

    Add labels

--- src/components/SkillRankInput.js
+++ src/components/SkillRankInput.js
@@ -16,19 +16,20 @@ export default class SkillRankInput extends Component {
 			canPurchaseNextRank
 		} = this.props
 
-		console.log( nextCost, "canPurchaseNextRank", canPurchaseNextRank );
-		
+		const labelClass = "label " + skillCharacteristic.toLowerCase();
+
+
 		return (
 			<div>
-				<form className = "form-horizontal col-sm-6">
+				<form className = "form-horizontal col-sm-6 col-md-4 col-lg-3 ">
 				<div className = "form-group">
-					<label className = "control-label col-sm-4">{skillName} [{skillCharacteristic}]</label>
-					<div className="btn-group col-sm-8" role="group">
-						<button type="button" className="btn btn-default">1</button>
-						<button type="button" className="btn btn-default">2</button>
-						<button type="button" className="btn btn-default">3</button>
-						<button type="button" className="btn btn-default">4</button>
-						<button type="button" className="btn btn-default">5</button>
+					<div className = "btn-group col-sm-8" role = "group">
+						<h3><span className = { labelClass }>{skillName}</span></h3>
+						<button type = "button" className = "btn btn-default">1</button>
+						<button type = "button" className = "btn btn-default">2</button>
+						<button type = "button" className = "btn btn-default">3</button>
+						<button type = "button" className = "btn btn-default">4</button>
+						<button type = "button" className = "btn btn-default">5</button>
 					</div>
 				</div>
 				</form>
